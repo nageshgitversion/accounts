02249049b2e752f707e1a94dc5f1f847e2f89657
nageshgitversion <149996541+nageshgitversion@users.noreply.github.com>
2025-12-17 20:47:32 +0530

    Fix greeting message in AccountsServiceImpl

--- src/main/java/com/minibank/accounts/service/Impl/AccountsServiceImpl.java
+++ src/main/java/com/minibank/accounts/service/Impl/AccountsServiceImpl.java
@@ -27,7 +27,7 @@ public class AccountsServiceImpl implements IAccountsService {
     public AccountsServiceImpl(AccountsRepository accountsRepository, CustomerRepository customerRepository) {
         this.accountsRepository = accountsRepository;
         this.customerRepository = customerRepository;
-        System.out.println("hello every");
+        System.out.println("hello everyone");
     }
 
     @Override
